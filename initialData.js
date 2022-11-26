3b0a1e8b9d73281635f77fdedf7fc11a2c5e91a4
saedMuhamed <saedxabo@gmail.com>
2022-11-26 18:39:25 +0300

    Adding Files to remote repo

---
+++ initialData.js
@@ -0,0 +1,13 @@
+const users = [
+  {
+    _id: 1587912,
+    api_key: "5iyx82xb4smpsoec1v6odengm7b2b4",
+    username: "username",
+    usage: [{ date: "2022-10-10", count: 17 }],
+  },
+];
+const Countries = [
+  { _id: 1, name: "Nigeria" },
+  { _id: 2, name: "China" },
+];
+module.exports = { users, Countries };
